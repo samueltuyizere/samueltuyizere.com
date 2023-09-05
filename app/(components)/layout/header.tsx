8678b7c9955a328e1ad77e050bc63711a81aaf60
TUYIZERE Samuel <samueltuyizere0@gmail.com>
2023-09-05 16:24:28 +0200

    added a header

---
+++ app/(components)/layout/header.tsx
@@ -0,0 +1,34 @@
+export default function Header(){
+    return(
+        <>
+        <nav x-data="{ isOpen: false }" className="relative bg-white dark:bg-gray-900">
+        <div className="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
+            <div className="flex items-center justify-between">
+                <a href="/">
+                    {/* TODO: add my logo / image here */}
+                </a>
+    
+                <div className="flex md:hidden">
+                    <button x-cloak type="button" className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
+                        <svg x-show="!isOpen" xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M4 8h16M4 16h16" />
+                        </svg>
+
+                        <svg x-show="isOpen" xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                            stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+    
+            <div x-cloak className="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-900 md:bg-transparent md:dark:bg-transparent md:mt-0 md:p-0 md:top-0 md:relative md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center">
+                <div className="flex flex-col md:flex-row md:mx-6">
+                    <a className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="mailto:sam@ejobu.rw">Contact Me</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+        </>
+    )
+}
